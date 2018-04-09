342cf0e0e1bf09f79ad4a5424d4b7357637ac4f9
Do Ha Long <dohalong1993@gmail.com>
2018-04-09 16:50:57 +0700

    Change structure

--- src/main/java/com/springsocialexample/controllers/LoginController.java
+++ src/main/java/com/springsocialexample/controllers/LoginController.java
@@ -1,46 +1,38 @@
 package com.springsocialexample.controllers;
 
 import com.springsocialexample.exceptions.ProviderConnectionException;
-import com.springsocialexample.models.Result;
-import com.springsocialexample.models.UserBean;
-import com.springsocialexample.services.BaseProviderService;
-import com.springsocialexample.utility.ResultCode;
+import com.springsocialexample.services.FacebookProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.connect.ConnectionFactoryLocator;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.connect.UsersConnectionRepository;
-import org.springframework.social.connect.web.ConnectController;
-import org.springframework.social.connect.web.ProviderSignInUtils;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.social.facebook.api.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 public class LoginController {
-    @Autowired
-    BaseProviderService facebookProviderService;
-
-    private final ProviderSignInUtils signInUtils;
+    private FacebookProviderService facebookProviderService;
 
     @Autowired
-    public LoginController(ConnectionFactoryLocator connectionFactoryLocator, UsersConnectionRepository connectionRepository) {
-        signInUtils = new ProviderSignInUtils(connectionFactoryLocator, connectionRepository);
+    public LoginController(FacebookProviderService facebookProviderService) {
+        this.facebookProviderService = facebookProviderService;
+    }
+
+    @GetMapping("login/facebook")
+    public String loginViaFacebook() {
+        try {
+            return facebookProviderService.createAuthorizationURL("http://localhost:8080/facebook", null);
+        } catch (ProviderConnectionException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 
     @GetMapping("/facebook")
-    public Result<UserBean> loginViaFacebook(){
-        try {
-            ProviderSignInUtils
-            UserBean userBeanData = facebookProviderService.getUserBeanData();
-            return new Result<>(ResultCode.OK.getCode(), ResultCode.OK.getMessage(), userBeanData);
-        } catch (ProviderConnectionException e) {
-            return new Result<>(e.getErrorCode(), e.getMessage(), null);
-        } catch (Exception e) {
-            return new Result<>(e.getMessage(), ResultCode.FAIL_UNRECOGNIZED_ERROR.getMessage(), null);
-        }
+    public String createFacebookAccessToken(@RequestParam("code") String code){
+        return facebookProviderService.getAccessToken(code, "http://localhost:8080/facebook");
+    }
+
+    @GetMapping("/getName")
+    public User getNameResponse(){
+        return facebookProviderService.getName();
     }
 }
--- src/main/java/com/springsocialexample/services/FacebookProviderService.java
+++ src/main/java/com/springsocialexample/services/FacebookProviderService.java
@@ -2,23 +2,41 @@ package com.springsocialexample.services;
 
 import com.springsocialexample.exceptions.ProviderConnectionException;
 import com.springsocialexample.models.UserBean;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.User;
+import org.springframework.social.facebook.api.impl.FacebookTemplate;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FacebookProviderService extends BaseProviderService{
+@Getter
+public class FacebookProviderService implements BaseProviderService{
     private static final String FACEBOOK = "facebook";
 
-    @Override
-    public UserBean getUserBeanData() throws ProviderConnectionException{
-        connectViaProvider(Facebook.class);
-        return populateUserDetailsFromProvider();
+    private String accessToken;
+    private OAuth2Operations oAuth2Operations;
+
+    public FacebookProviderService(@Value("${spring.social.facebook.appId}") String facebookAppId,
+            @Value("${spring.social.facebook.appSecret}") String facebookSecret) {
+        this.oAuth2Operations = new FacebookConnectionFactory(facebookAppId, facebookSecret).getOAuthOperations();
     }
 
     @Override
-    protected UserBean populateUserDetailsFromProvider() {
-        Facebook facebook = baseProvider.getFacebook();
+    public String createAuthorizationURL(String url, String scope) throws ProviderConnectionException {
+        OAuth2Parameters params = new OAuth2Parameters();
+        params.setRedirectUri(url);
+        params.setScope(scope);
+        return getOAuth2Operations().buildAuthorizeUrl(params);
+    }
+
+    @Override
+    public UserBean populateUserDetailsFromProvider(Facebook facebook) {
         User user = facebook.userOperations().getUserProfile();
         UserBean userBean = new UserBean();
         userBean.setEmail(user.getEmail());
@@ -27,5 +45,22 @@ public class FacebookProviderService extends BaseProviderService{
         userBean.setImage(user.getCover().getSource());
         userBean.setProvider(FACEBOOK);
         return userBean;
+    }
+
+    public String getAccessToken(String code, String url) {
+        try{
+            AccessGrant accessGrant = getOAuth2Operations().exchangeForAccess(code, url, null);
+            accessToken = accessGrant.getAccessToken();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return getAccessToken();
+    }
+
+    public User getName() {
+        Facebook facebook = new FacebookTemplate(accessToken);
+        String[] fields = {"name", "email", "timezone"};
+        return facebook.fetchObject("me", User.class, fields);
     }
 }
